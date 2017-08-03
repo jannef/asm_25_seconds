3157950f6ac4f30bc3804d1ec5ccd9f64340459b
Teemu Kokkonen <teemu@rautasydan.org>
2017-08-04 02:46:07 +0300

    Map display and generation, exits and keys, temporary WASD movement, Android build settings

---
+++ asm_25_dungeon/Assets/Scripts/MapManager.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+    static MapManager _instance;
+    public static MapManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new MapManager();
+            }
+            return _instance;
+        }
+    }
+
+
+    [Multiline(8)]
+    public string MapInput = "";
+    public TileBind[] TileDefinitions;
+    public Player Player;
+
+    bool _exitUnlocked;
+    char[][] _mapArray;
+    TileBind[][] _mapTiles;
+
+	// Use this for initialization
+	void Start () {
+        _instance = this;
+        _mapArray = ParseMapString(MapInput);
+        _mapTiles = ParseMapChars(_mapArray);
+        SpawnTiles(_mapTiles);
+        PlacePlayer();
+	}
+
+    private void PlacePlayer()
+    {
+        if (Player != null)
+        {
+            Instantiate(Player.transform, GetTileScenePosition(Player.PositionTileX, Player.PositionTileY), Quaternion.identity);
+        } else
+        {
+            Debug.LogWarning("Player not defined for MapManager");
+        }
+    }
+
+    private TileBind[][] ParseMapChars(char[][] charMap)
+    {
+        TileBind[][] tileMap = new TileBind[charMap.Length][];
+        for(int i = 0; i < charMap.Length; i++)
+        {
+            // Initialize tile map row
+            tileMap[i] = new TileBind[charMap[i].Length];
+            // Populate tile map row
+            for(int j = 0; j < charMap[i].Length; j++)
+            {
+                // Find corresponding tile and put it to the array
+                foreach(TileBind t in TileDefinitions)
+                {
+                    if (t.Key.Equals(charMap[i][j]))
+                    {
+                        tileMap[i][j] = t;
+                        break;
+                    }
+                }
+            }
+        }
+        return tileMap;
+    }
+
+    internal void Exit()
+    {
+        if (_exitUnlocked)
+        {
+            // TODO next level
+            Debug.Log("Level complete!");
+        } else
+        {
+            // TODO prompt
+            Debug.LogFormat("<color='orange'>Find the key!</color>");
+        }
+    }
+
+    internal void UnlockExit(bool unlock)
+    {
+        _exitUnlocked = unlock;
+    }
+
+    char[][] ParseMapString(string mapStr)
+    {
+        char[][] parsedMap;
+        string[] rows = mapStr.Split('\n');
+        parsedMap = new char[rows.Length][];
+        for (int i = 0; i < parsedMap.Length; i++)
+        {
+            parsedMap[i] = rows[i].Trim('\r').ToCharArray();
+        }
+
+        return parsedMap;
+    }
+
+    float GetScaleFactor(char[][] charMap)
+    {
+
+        return 1.0f;
+    }
+
+    void SpawnTiles(TileBind[][] tileMap)
+    {
+        Vector3 spawnPosition = new Vector3();
+        for (int i = 0; i < tileMap.Length; i++)
+        {
+            for(int j = 0; j < tileMap[i].Length; j++)
+            {
+                if (tileMap[i][j].Prefab != null)
+                {
+                    spawnPosition.x = i;
+                    spawnPosition.z = j;
+                    Instantiate(tileMap[i][j].Prefab, spawnPosition, Quaternion.identity);
+                }
+
+            }
+        }
+    }
+
+    public TileBind GetTileAt(int x, int y)
+    {
+        TileBind tile;
+        try
+        {
+            tile = _mapTiles[x][y];
+        }
+        catch
+        {
+            tile = new TileBind();
+        }
+        return tile;
+    }
+    public Vector3 GetTileScenePosition(int x, int y)
+    {
+        return new Vector3(x, 0, y);
+    }
+}
+
+[Serializable]
+public struct TileBind
+{
+    public char Key;
+    public Transform Prefab;
+}
---
+++ asm_25_dungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public enum MovementEvent { None, Blocked, Pass, Enemy, Item, Exit }
+    public int PositionTileX = 0;
+    public int PositionTileY = 0;
+    public int MoveLimit = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Move(0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(0, -1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(-1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(1, 0);
+        }
+	}
+
+    public void Move(float directionX, float directionY)
+    {
+        directionX = Mathf.Sign(directionX);
+        directionY = Mathf.Sign(directionY);
+        Move((int)directionX, (int)directionY);
+    }
+    public void Move(int directionX, int directionY)
+    {
+        int targetTileX = PositionTileX;
+        int targetTileY = PositionTileY;
+        if (directionX == 0 && directionY == 0)
+        {
+            return;
+        }
+        if(directionX != 0 && directionY != 0)
+        {
+            directionX = 0;
+        }
+
+        int steps = 0;
+        TileBind targetTile;
+        while (IsPassable(targetTile = MapManager.Instance.GetTileAt(targetTileX+directionX, targetTileY+directionY)) && (steps < MoveLimit || MoveLimit < 0))
+        {
+            targetTileX += directionX;
+            targetTileY += directionY;
+            steps++;
+        }
+
+        switch (CheckTileForEvent(targetTile))
+        {
+            case MovementEvent.Item:
+                MapManager.Instance.UnlockExit(true);
+                break;
+            case MovementEvent.Exit:
+                MapManager.Instance.Exit();
+                break;
+            case MovementEvent.Enemy:
+                Fight(targetTile);
+                break;
+        }
+
+        transform.position = MapManager.Instance.GetTileScenePosition(targetTileX, targetTileY);
+        PositionTileX = targetTileX;
+        PositionTileY = targetTileY;
+    }
+
+    private bool IsPassable(TileBind tile)
+    {
+        return CheckTileForEvent(tile) == MovementEvent.Pass;
+    }
+    private MovementEvent CheckTileForEvent(TileBind tile)
+    {
+        // TODO set and fetch event from the tile's object
+        if (tile.Key.Equals('*'))
+        {
+            return MovementEvent.Pass;
+        }
+        else if (tile.Key.Equals('#'))
+        {
+            return MovementEvent.Exit;
+        } else if (tile.Key.Equals('$'))
+        {
+            return MovementEvent.Item;
+        }
+        else
+        {
+            return MovementEvent.Blocked;
+        }
+    }
+
+    private void Fight(TileBind enemyTile)
+    {
+        // TODO Encounter
+        Debug.LogFormat("[color='red']ENEMY[/color]");
+    }
+}
