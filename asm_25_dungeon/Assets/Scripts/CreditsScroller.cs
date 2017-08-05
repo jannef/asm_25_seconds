484e9ecc85f1d6f7283a2f69195d8b73b08a47b4
Janne Forsell <jannef1337@gmail.com>
2017-08-05 20:18:46 +0300

    Auto stash before rebase of "origin/master"

---
+++ asm_25_dungeon/Assets/Scripts/CreditsScroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScroller : MonoBehaviour
+{
+    [SerializeField] public string[] CreditNames;
+    [SerializeField] public Text TextField;
+    public float LetterDelay = 0.1f;
+    public float NameDelay = 1.0f;
+
+    private void Awake()
+    {
+        StartCoroutine(DisplayName(0, CreditNames));
+    }
+
+    private IEnumerator DisplayName(int index, string[] names)
+    {
+        var name = "";
+        foreach (var c in names[index])
+        {
+            yield return new WaitForSeconds(LetterDelay);
+            name += c.ToString();
+            TextField.text = name;
+        }
+
+        yield return new WaitForSeconds(NameDelay);
+        StartCoroutine(DisplayName((index + 1) % names.Length, names));
+    }
+}
